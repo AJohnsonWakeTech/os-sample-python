386c817f76bc80a0363a6437a46ec87b16a9ae69
AJohnsonWakeTech <75625961+AJohnsonWakeTech@users.noreply.github.com>
2021-05-01 08:26:19 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    window = Tk()
+    window.title("Welcome to LikeGeeks app")
+    window.mainloop()
     return "Hello World!"
 
 if __name__ == "__main__":
